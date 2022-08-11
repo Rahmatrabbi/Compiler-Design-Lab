ad655b2b2fc1738313c80f7bc867d418e7f5c93b
Rahmatrabbi <48399993+Rahmatrabbi@users.noreply.github.com>
2022-08-11 11:30:35 +0800

    This is problem 3
    abbreviates

---
+++ problem3.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+//#include<stdio.h>
+int main()
+{
+    freopen("in.txt","r",stdin);
+    freopen("out.txt","w",stdout);
+    int i,len,cnt=0;
+    string str;
+    cin>>str;
+    len=str.length();
+    if(str[0]=='C'&&str[1]=='S'&&str[2]=='E'&&str[3]=='-'&&str[4]=='3'&&str[5]=='1'&&str[6]=='4'&&str[7]=='1')
+    {
+        printf("Computer Science & Engineering,3rd year,1st semester,Compiler Design ,Theory");
+    }
+}
